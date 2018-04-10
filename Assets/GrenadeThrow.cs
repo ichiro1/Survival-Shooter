1ca649c1337bd724a2abea57c57d9b7f69453b59
UCode <UCode@UCODE-TPV-GAMMA.fios-router.home>
2018-04-10 16:18:12 -0700

    Grenades

---
+++ Assets/GrenadeThrow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrenadeThrow : MonoBehaviour {
+	public float throwForce = 10f;
+	public GameObject grenade;
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("g")) {
+			ThrowGrenade ();
+		}
+	}
+	void ThrowGrenade() {
+		GameObject grenadethrow = Instantiate (grenade, transform.position, transform.rotation);
+
+			Rigidbody rb = grenadethrow.GetComponent<Rigidbody> ();
+			rb.AddForce (transform.forward * throwForce, ForceMode.VelocityChange);
+
+	}
+}
